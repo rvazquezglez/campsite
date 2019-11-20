023b6f02d0e641647b1057b97d53d22a57cabf53
rvazquezglez <rvazquezglez@gmail.com>
2019-11-20 15:41:34 -0600

    Initial commit

---
+++ src/main/java/com/upgrade/campsite/CampsiteApplication.java
@@ -0,0 +1,13 @@
+package com.upgrade.campsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upgrade/campsite/CampsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upgrade.campsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
